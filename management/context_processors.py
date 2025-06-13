4ea8e485ba256be42bed45d5eee85441eca56f2a
ashish <ashish.kekaan@lemonlabs.in>
2025-06-13 12:24:37 +0530

    Fixed desings

--- management/context_processors.py
+++ management/context_processors.py
@@ -4,9 +4,10 @@ from management.models import RecentActivity
 
 
 def today_notifications_count(request):
-    if request.user.is_authenticated:
+    user = request.user
+    if user.is_authenticated:
         count = RecentActivity.objects.filter(
-            user=request.user, read=False, timestamp__date=date.today()
+            user=user, read=False, timestamp__date=date.today()
         ).count()
         return {"today_notification_count": count}
     return {"today_notification_count": 0}
